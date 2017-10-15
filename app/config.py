1c170060d436e6d52aab1decda0281a76d827383
SmellyCat <smellycat2014@foxmail.com>
2017-10-15 17:13:09 +0800

    添加 配置文件

--- app/config.py
+++ app/config.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
-
+from . import helper_kafka
 
 class Config(object):
     # 密码 string
-    SECRET_KEY = 'hellokitty'
+    SECRET_KEY = 'showmethemoney'
     # 服务器名称
     HEADER_SERVER = 'SX-ParkingLotServer'
     # 加密次数 int
@@ -11,17 +11,19 @@ class Config(object):
     # token生存周期，默认2小时 int
     EXPIRES = 7200
     # 数据库连接 string
-    SQLALCHEMY_DATABASE_URI = 'mysql://root:root@127.0.0.1/parking'
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///../kakou.db'
     # 数据库连接绑定 dict
     SQLALCHEMY_BINDS = {}
     # 连接池大小 int
-    #SQLALCHEMY_POOL_SIZE = 5
+    # SQLALCHEMY_POOL_SIZE = 5
     # 用户权限范围 dict
     SCOPE_USER = {}
     # 白名单启用 bool
     WHITE_LIST_OPEN = True
     # 白名单列表 set
     WHITE_LIST = set()
+    # kafka实例
+    KA = helper_kafka.KafkaData(**{'services': '10.123.123.123:9092', 'topic': 'kakou'})
 
 
 class Develop(Config):
